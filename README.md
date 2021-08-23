59d86faac881d6eb602a8018f2048cdbbe271f18
Rasla <programmer.rasla@gmail.com>
2021-08-24 00:08:10 +0530

    Added mogno db atlas connection

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-#Full stack blog using Mongo, Node and Express
+# Full stack blog using Mongo, Node and Express
+
+## Instructions
+
+* **__run "npm i" to install dependencies__**
+* **__"run npm run devStart" to run the server__**
+
+**Thanks**
--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 
 const app = express();
-mongoose.connect("mongodb://localhost/blog", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose.connect(
+  "mongodb+srv://blog:ZC6xSHZN8XVre1dC@cluster0.ii5mt.mongodb.net/Cluster0?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
 
 blogRouter.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/public"));
